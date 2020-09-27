12e5b2ddad4498bd4d930ac4e27d03c9bef45b89
owenbean400 <owenbean400@gmail.com>
2020-09-27 16:49:42 -0400

    fix map icon?

--- src/components/map/mapIcon.js
+++ src/components/map/mapIcon.js
@@ -4,8 +4,8 @@ import 'react-leaflet';
 import ConeIcon from '../../images/cone_icon.svg';
 
 
-delete L.Icon.Default.prototype._getIconUrl;
-L.Icon.Default.mergeOptions({
+//delete L.Icon.Default.prototype._getIconUrl;
+/*L.Icon.Default.mergeOptions({
   iconRetinaUrl: require("../../images/cone_icon.svg"),
   iconUrl: require("../../images/cone_icon.svg"),
   shadowUrl: require("../../images/cone_icon_shadow.svg"),
@@ -14,9 +14,9 @@ L.Icon.Default.mergeOptions({
   iconSize: [100, 100],
   shadowSize: [68, 95],
   shadowAnchor: [-2, 74]
-});
+});*/
 
-const iconCone = new L.Icon({
+const iconCone = new L.Icon.extend({
   iconRetinaUrl: require("../../images/cone_icon.svg"),
   iconUrl: require("../../images/cone_icon.svg"),
   shadowUrl: require("../../images/cone_icon_shadow.svg"),
