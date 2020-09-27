f1785e30c716197dc6696faff136a9ec3b8cad4d
owenbean400 <owenbean400@gmail.com>
2020-09-27 16:41:40 -0400

    fix map icon?

--- src/components/map/mapIcon.js
+++ src/components/map/mapIcon.js
@@ -4,7 +4,7 @@ import ConeIcon from '../../images/cone_icon.svg';
 
 
 //delete L.Icon.Default.prototype._getIconUrl;
-L.Icon.Default.mergeOptions({
+/*L.Icon.Default.mergeOptions({
   iconRetinaUrl: require("../../images/cone_icon.svg"),
   iconUrl: require("../../images/cone_icon.svg"),
   shadowUrl: require("../../images/cone_icon_shadow.svg"),
@@ -13,7 +13,7 @@ L.Icon.Default.mergeOptions({
   iconSize: [100, 100],
   shadowSize: [68, 95],
   shadowAnchor: [-2, 74]
-});
+});*/
 
 const iconCone = new L.Icon({
   iconRetinaUrl: require("../../images/cone_icon.svg"),
